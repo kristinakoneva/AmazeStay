da36fa22a5f722fce862a5a07f12cb6551014347
Kristina Koneva <koneva.kristinaa@gmail.com>
2022-11-22 11:53:18 +0100

    Create a HomeController

---
+++ Homework2/technical_prototype/src/main/java/mk/ukim/finki/technical_prototype/web/HomeController.java
@@ -0,0 +1,15 @@
+package mk.ukim.finki.technical_prototype.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value={"","/"})
+public class HomeController {
+
+    @GetMapping
+    public String getHomePage(){
+        return "home_page.html";
+    }
+}
