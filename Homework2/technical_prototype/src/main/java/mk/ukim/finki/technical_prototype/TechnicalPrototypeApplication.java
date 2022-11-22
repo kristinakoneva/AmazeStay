df24f1095440b5d061142010d14266c1864ce61b
Kristina Koneva <koneva.kristinaa@gmail.com>
2022-11-22 10:49:51 +0100

    Create Homework2 folder and its subfolders, setup gitignore

---
+++ Homework2/technical_prototype/src/main/java/mk/ukim/finki/technical_prototype/TechnicalPrototypeApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.technical_prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalPrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicalPrototypeApplication.class, args);
+    }
+
+}
---
+++ Homework2/technical_prototype/src/test/java/mk/ukim/finki/technical_prototype/TechnicalPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.technical_prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalPrototypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
