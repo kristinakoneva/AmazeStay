f80e4410063948a1ef7de82b11fe8703e662b47b
Kristina Koneva <koneva.kristinaa@gmail.com>
2022-12-24 19:01:50 +0100

    Initial fix to add-comment form

--- Homework3/amaze_stay/src/main/java/mk/ukim/finki/amaze_stay/config/WebSecurityConfig.java
+++ Homework3/amaze_stay/src/main/java/mk/ukim/finki/amaze_stay/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,6 +23,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         this.passwordEncoder = passwordEncoder;
         this.authenticationProvider = authenticationProvider;
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/resources/**");
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
